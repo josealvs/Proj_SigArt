e9f0dd558e00662295623872abb708b7be9cd074
josealvs <j.anjos@escolar.ifrn.edu.br>
2025-03-28 17:44:13 -0300

    test: First Task Test

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import status
+from rest_framework import status
+from .models import Task
 
+class TaskAPITestCase(APITestCase):
+
+    def setUp(self):
+        self.task = Task.objects.create(
+            title = "Primeira Tarefa",
+            description = "Tarefa Teste",
+            due_date = "2025-04-01",
+            status = "new",
+        )
+        
 # Create your tests here.
